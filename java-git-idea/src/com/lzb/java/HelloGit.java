3979a2f5f5ee1e9b4ec082d29bff1c9ff2a174bd
root <123456>
2020-12-03 11:20:24 +0800

    王大

--- java-git-idea/src/com/lzb/java/HelloGit.java
+++ java-git-idea/src/com/lzb/java/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("Hello Git");
         System.out.println("hghjgj");
         System.out.println("update 1");
+        System.out.println("王大");
     }
 }
