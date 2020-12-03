fd5a0ac6c1158cd8fc4fd7b7eb0e1cf1044223bf
root <123456>
2020-12-03 10:37:45 +0800

    初始文件

---
+++ java-git-idea/src/com/lzb/java/HelloGit.java
@@ -0,0 +1,11 @@
+package com.lzb.java;
+
+/**
+ * @author
+ * @create 2020-12-03-10:36
+ */
+public class HelloGit {
+    public static void main (String[] args) {
+        System.out.println("Hello Git");
+    }
+}
